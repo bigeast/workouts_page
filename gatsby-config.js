2bc0e16e3819961124a7ca940e0f7b79676900ce
bigeast <dadongking@gmail.com>
2022-05-02 19:40:03 +0800

    bigeast init commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,17 +2,13 @@ module.exports = {
   pathPrefix: '/', // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'Workouts Map',
-    siteUrl: 'https://ben29.xyz',
-    logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+    siteUrl: 'https://bigeast.github.io/workouts_page',
+    logo: 'https://dgalywyr863hv.cloudfront.net/pictures/athletes/71080136/24301448/1/large.jpg',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://ben29.xyz',
-      },
-      {
-        name: 'About',
-        url: 'https://github.com/ben-29/running_page/blob/master/README-CN.md',
+        url: 'https://bigeast.github.io/workouts_page',
       },
     ],
   },
